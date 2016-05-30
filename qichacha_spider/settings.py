18640a7fb333f85d1651a8032916d841f575c279
mingz <305603665@qq.com>
2016-05-30 14:01:44 +0800

    qichacha

--- qichacha_spider/settings.py
+++ qichacha_spider/settings.py
@@ -71,8 +71,8 @@ ROBOTSTXT_OBEY = True
 # CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-# COOKIES_ENABLED = False
-COOKIES_DEBUG = True
+COOKIES_ENABLED = False
+# COOKIES_DEBUG = True
 
 # Disable Telnet Console (enabled by default)
 # TELNETCONSOLE_ENABLED = False
