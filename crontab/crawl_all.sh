0074b48fe877680e78392fd5e0a7dc3adccead22
mingz <305603665@qq.com>
2016-05-30 18:04:05 +0800

    qichacha

--- crontab/crawl_all.sh
+++ crontab/crawl_all.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 cd /home/apps/qichacha_spider
-dt=$(date "+%Y-%m-%d")
-path_to_log="log/""$dt""_crawl_all.log"
-echo $path_to_log
-scrapy mc_crawl_all -s LOG_FILE=$path_to_log &
+sh crawl_bjda.sh &
+sh crawl_jd.sh &
