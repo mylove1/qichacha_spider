8e6489d1a524548a1769f382501a1625749ffc3d
mingz <305603665@qq.com>
2016-05-30 13:58:07 +0800

    qichacha

--- qichacha_spider/settings.py
+++ qichacha_spider/settings.py
@@ -96,9 +96,11 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    'qichacha_spider.middlewares.MyCustomDownloaderMiddleware': 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+    # 'qichacha_spider.middlewares.MyCustomDownloaderMiddleware': 543,
+    'qichacha_spider.middlewares.ProxyMiddleware': 760,
+    # 'qichacha_spider.middlewares.RandomUserAgentMiddleware': 410,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
