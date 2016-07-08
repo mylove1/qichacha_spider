e81a3f4fa03c93fb0755fe79463e078bea002052
mingz <305603665@qq.com>
2016-07-08 22:11:37 +0800

    gb2312

--- qichacha_spider/spiders/qichacha_spider_gb2312.py
+++ qichacha_spider/spiders/qichacha_spider_gb2312.py
@@ -21,7 +21,8 @@ class QichachaSpider(scrapy.Spider):
         for i in range(len(txt)):
             for j in range(len(txt)):
                 search_key = txt[i] + txt[j]
-                print "++++++gb2312+++++++:", i, j, len(txt), search_key
+                print "++++++gb2312+++++++:", time.strftime('%Y-%m-%d', time.localtime(time.time())), i, j, len(
+                    txt), search_key
                 url = "http://www.qichacha.com/search?key=" + urllib.quote(search_key.encode('utf-8')) + "&index=0"
                 print url
                 request = scrapy.Request(
