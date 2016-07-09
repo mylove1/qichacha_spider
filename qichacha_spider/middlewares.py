d8c630b57672f55e119e6673c6d3ab372a4a0365
mingz <305603665@qq.com>
2016-07-09 15:34:31 +0800

    gb2312

--- qichacha_spider/middlewares.py
+++ qichacha_spider/middlewares.py
@@ -30,11 +30,11 @@ class ProxyMiddleware(object):
 
     def process_request(self, request, spider):
 
-        if request.url.startswith("https://"):
-            # TODO 由于https代理验证有问题,这里暂时改为,将https代理替换成http代理
-            url = request.url.replace("https://", "http://")
-            print "replace url, ", url
-            request.replace(url=url)
+        # if request.url.startswith("https://"):
+        #     # TODO 由于https代理验证有问题,这里暂时改为,将https代理替换成http代理
+        #     url = request.url.replace("https://", "http://")
+        #     print "replace url, ", url
+        #     request.replace(url=url)
 
             # n = ProxyMiddleware.https_n
             # if n >= len(ProxyMiddleware.proxy_items_https):
@@ -46,20 +46,21 @@ class ProxyMiddleware(object):
             # ProxyMiddleware.https_n = n + 1
 
         # Set the location of the proxy
-        if request.url.startswith("http://"):
-            proxy_len = len(ProxyMiddleware.proxy_items_http)
-            if proxy_len > 0:
-                n = ProxyMiddleware.http_n
-                if n >= len(ProxyMiddleware.proxy_items_http):
-                    ProxyMiddleware.proxy_items_http = ProxyHelper.get_proxy_items_type_http()
-                    n = 0
+        # if request.url.startswith("http://"):
+        proxy_len = len(ProxyMiddleware.proxy_items_http)
+        if proxy_len > 0:
+            n = ProxyMiddleware.http_n
+            if n >= len(ProxyMiddleware.proxy_items_http):
+                ProxyMiddleware.proxy_items_http = ProxyHelper.get_proxy_items_type_http()
+                n = 0
 
-                request.meta['proxy'] = "http://%s:%d" % (
-                    ProxyMiddleware.proxy_items_http[n]["ip"], int(ProxyMiddleware.proxy_items_http[n]["port"]))
-                # ProxyMiddleware.http_n = n + 1
+            request.meta['proxy'] = "http://%s:%d" % (
+                ProxyMiddleware.proxy_items_http[n]["ip"], int(ProxyMiddleware.proxy_items_http[n]["port"]))
+            ProxyMiddleware.http_n = n + 1
+            print request.meta['proxy']
 
-                # # Use the following lines if your proxy requires authentication
-                # proxy_user_pass = "USERNAME:PASSWORD"
-                # # setup basic authentication for the proxy
-                # encoded_user_pass = base64.b64encode(proxy_user_pass)
-                # request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
+            # # Use the following lines if your proxy requires authentication
+            # proxy_user_pass = "USERNAME:PASSWORD"
+            # # setup basic authentication for the proxy
+            # encoded_user_pass = base64.b64encode(proxy_user_pass)
+            # request.headers['Proxy-Authorization'] = 'Basic ' + encoded_user_pass
